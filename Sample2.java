45c0f58dd393ad96d2b261bbd7316e33896ccf94
KJLEE <kangjun.lee0924@gmail.com>
2025-02-26 13:32:49 +0900

    rename

--- Sample2.java
+++ Sample2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class sample2 {
+public class Sample2 {
     public static void main(String[] args) {
 //        arraySample();
 //        scannerSample();
